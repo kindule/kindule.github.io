5e7171202cb28be93a1a8f193b0f3e7e942662c6
ge.jin <ge.jin@yunqutech.com>
2024-12-14 12:19:15 +0800

    Create hugo.yaml

---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "只是为了好玩 \u003e Posts",
+    "content": "Introduction This is bold text, and this is emphasized text.\nVisit the Hugo website!",
+    "description": "Introduction This is bold text, and this is emphasized text.\nVisit the Hugo website!",
+    "tags": [],
+    "title": "First",
+    "uri": "/posts/first/index.html"
+  },
+  {
+    "breadcrumb": "只是为了好玩",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "只是为了好玩",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "只是为了好玩",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "只是为了好玩",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
